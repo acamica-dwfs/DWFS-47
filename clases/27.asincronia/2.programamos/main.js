c5386e8b9edfa49d682feeccc16a412fa76c6f6d
Yulian andres zapata m <hash.key66@gmail.com>
2020-10-29 18:56:44 -0500

    ejercicios de asincronia

---
+++ clases/27.asincronia/2.programamos/main.js
@@ -0,0 +1,17 @@
+/**
+ *
+ Hemos realizado las acciones de sumar y restar mediante un callback.
+ Es momento de añadir las funciones de multiplicar y dividir.
+ Luego, añade el promedio de ambos utilizando una función inline.
+ */
+
+let suma = (n1, n2) => n1 + n2;
+let resta = (n1, n2) => n1 - n2;
+let mult = (n1, n2) => n1 * n2;
+let div = (n1, n2) => n1 / n2;
+
+let calculadora = function (n1, n2, callback){
+    return callback(n1, n2);
+};
+
+console.log(calculadora(7, 7, mult));
