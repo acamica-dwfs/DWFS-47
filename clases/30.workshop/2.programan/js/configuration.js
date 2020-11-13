b5951ccdedf4d6ccaf7dfc1247269a7bbaa296d6
Yulian andres zapata m <hash.key66@gmail.com>
2020-11-12 19:42:07 -0500

    citas

---
+++ clases/30.workshop/2.programan/js/configuration.js
@@ -0,0 +1,2 @@
+//const api = 'VhZH68eUeluE17m4y9UW4Wq7QZI9rLRd';
+const BASE_URL = 'https://thesimpsonsquoteapi.glitch.me';
---
+++ clases/30.workshop/2.programan/js/main.js
@@ -0,0 +1,61 @@
+let searchBtn = document.getElementById('searchBtn');
+let trendingBtn = document.getElementById('trending');
+let searchInput = document.getElementById('search');
+let pokeCtn = document.getElementById('pokeCtn');
+
+
+/**
+ * tendencia  de gifs
+ * @param limit ::  limite de elementos
+ */
+const trending = (limit) => {
+    let url = `${BASE_URL}trending?api_key=${api}&limit=${limit}`;
+    let result = apiFetchGET(url);
+    result.then((resp) => {
+        pokeCtn.innerHTML = "";
+        resp.data.map(item => {
+            addToDOM(item);
+        })
+    }).catch((e) => {
+        console.log("a ocurrido un error" + e);
+    })
+}
+
+/**
+ *   Buscar un gif
+ * @param term ::  termino de busueda
+ */
+const searchGif = (term) => {
+    let url = `${BASE_URL}search?q=${term}&api_key=${api}`;
+    let result = apiFetchGET(url);
+    result.then((resp) => {
+        console.log("llamado a buscar gifs");
+        pokeCtn.innerHTML = "";
+        resp.data.map(item => {
+            addToDOM(item);
+        })
+    }).catch((e) => {
+        console.log("a ocurrido un error" + e);
+    })
+}
+
+/**
+ * agrega dinamicamente los elementos al html
+ * @param info
+ */
+const addToDOM = (info) => {
+    let ctn = document.createElement('div');
+    let img = document.createElement('img');
+    img.setAttribute('src', info.images["480w_still"].url);
+    ctn.appendChild(img);
+    pokeCtn.appendChild(ctn);
+}
+
+const  quotessimp = (limit) => {
+
+
+}
+
+trendingBtn.addEventListener('click', () => {
+    trending(4);
+});
