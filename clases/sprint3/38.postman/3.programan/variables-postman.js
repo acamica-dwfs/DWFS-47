d73f5d93f88a50cfe3a4e26a5f770cce12d9bcd2
Yulian andres zapata m <hash.key66@gmail.com>
2020-12-03 21:52:37 -0500

    programan

---
+++ clases/sprint3/38.postman/3.programan/variables-postman.js
@@ -0,0 +1,11 @@
+if (responseCode.code === 200) {
+    try {
+        var authorization = JSON.parse(responseBody).access_token;
+        postman.setEnvironmentVariable("token_acamica", authorization);
+    }
+    catch (e) {
+        console.log(e);
+        throw false;
+    }
+}
+
