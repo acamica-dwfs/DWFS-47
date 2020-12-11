4492c09fd74f5f2a042bb40286f8c1873d634e6c
Ifrain <ifrain.audiovisual@gmail.com>
2020-12-11 20:26:12 -0300

    actualización de ejercicio 3

--- clases/sprint3/39.nodejs/3.programan/ejemplo3/app.js
+++ clases/sprint3/39.nodejs/3.programan/ejemplo3/app.js
@@ -25,14 +25,13 @@ fs.writeFile('result.txt', `La suma es : \n ${a} + ${b} = ${calcu.suma(a, b)} \n
     console.log('esta es la suma');
 } )
 
-fs.appendFile('result.txt', `La resta es : \n ${a} - ${b} = ${calcu.resta(a, b)} \n \n`, ()=>{
-    console.log('esta es la resta');
-} )
+const fileAppen = (operación, resultado)=>{
+    fs.appendFile('result.txt', `La ${operación} es : ${resultado}`, ()=>{
+        console.log('esta es la resta');
+    } )
+}
 
-fs.appendFile('result.txt', `La multiplicación es : \n ${a} * ${b} = ${calcu.multi(a, b)} \n \n`, ()=>{
-    console.log('esta es la multiplicación');
-} )
-
-fs.appendFile('result.txt', `La división es : \n ${a} / ${b} = ${calcu.div(a, b)} \n \n`, ()=>{
-    console.log('y esta la división');
-} )
+fileAppen('suma', `\n ${a} + ${b} = ${calcu.suma(a, b)} \n \n`);
+fileAppen('suma', `\n ${a} - ${b} = ${calcu.resta(a, b)} \n \n`);
+fileAppen('suma', `\n ${a} * ${b} = ${calcu.multi(a, b)} \n \n`);
+fileAppen('suma', `\n ${a} / ${b} = ${calcu.div(a, b)} \n \n`);
