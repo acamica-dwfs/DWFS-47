325072333beb761d639ee38e45ac423401a08586
Yulian andres zapata m <hash.key66@gmail.com>
2020-12-11 09:39:38 -0500

    nodejs 1

--- clases/sprint3/39.nodejs/3.programan/ejemplo1/fs.js
+++ clases/sprint3/39.nodejs/3.programan/ejemplo1/fs.js
@@ -3,4 +3,8 @@ fs.writeFile('acamica.txt',"hola mundo",  () => {
     console.log(" archivo creado con contenido");
 });
 
+ fs.appendFile('acamica.txt'," \n " + 2,  () => {
+     console.log(" concatenar");
+ });
+
 
