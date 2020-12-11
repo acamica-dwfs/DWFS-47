c732d960a2da7a3b8226d0cc3399523b34e2329a
Yulian andres zapata m <hash.key66@gmail.com>
2020-12-11 09:48:12 -0500

    ejemplos nodejs2

---
+++ clases/sprint3/40.nodejs/1.programo/index.js
@@ -0,0 +1,11 @@
+/**
+ npm init
+ npm -v
+ npm install [packete]
+ npm install [packete] -g
+ npm uninstall
+ package.json
+ dotenv
+ Instalación global de nodemon
+ console.log (process.env.USER_DB):
+ */
