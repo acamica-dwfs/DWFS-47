59af27214a697d5cf2dc74f60c5bc13b372502ae
Yulian andres zapata m <hash.key66@gmail.com>
2021-01-12 12:53:44 -0500

    ejemplos de seguridad

--- clases/sprint3/44.seguridad/2.programamos/app.js
+++ clases/sprint3/44.seguridad/2.programamos/app.js
@@ -5,20 +5,22 @@ const rateLimit = require("express-rate-limit");
 
 const contactos = [
     { "id": 1, "nombre": "Dani" },
-    { "id": 2, "nombre": "Hugo" }, 
+    { "id": 2, "nombre": "Hugo" },
     { "id": 3, "nombre": "Juan" }
 ];
 
 //En una hora , Maximo 5 Request
 const checkLimite = rateLimit({
-    windowMs: 60 * 60 * 1000, 
-    max: 5, 
+    windowMs: 60 * 60 * 1000,
+    max: 5,
     message:
         "Supero el limite de 5 requests por hora, espera una hora para volver a comenzar"
 });
 
+app.use(checkLimite); // global
 
-app.get("/contactos", checkLimite, (req, res) => {
+
+app.get("/contactos", (req, res) => {
 
     respuesta = {
         error: false,
